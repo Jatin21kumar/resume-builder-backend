166ea88b3d568584fbd9318e52a3ab8276efdbea
Jatu <jatinkumar1721@gmail.com>
2026-01-17 23:11:18 +0530

    Allow vercel frontend origin in CORS config

--- src/main/java/com/jatin/resume_builder/config/SecurityConfig.java
+++ src/main/java/com/jatin/resume_builder/config/SecurityConfig.java
@@ -65,7 +65,10 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+		configuration.setAllowedOrigins(Arrays.asList(
+			    "http://localhost:5173",
+			    "https://resume-builder-frontend-steel.vercel.app"
+			));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
