45f5527921236c12288dc530b8c193f62d5b2b79
Jatu <jatinkumar1721@gmail.com>
2026-04-05 22:59:00 +0530

    Update security config CORS and public auth endpoints

--- src/main/java/com/jatin/resume_builder/config/SecurityConfig.java
+++ src/main/java/com/jatin/resume_builder/config/SecurityConfig.java
@@ -65,10 +65,10 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList(
-			    "http://localhost:5173",
-			    "https://resume-builder-frontend-steel.vercel.app"
-			));
+		configuration.setAllowedOriginPatterns(Arrays.asList(
+			"http://localhost:5173",
+			"https://*.vercel.app"
+		));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
